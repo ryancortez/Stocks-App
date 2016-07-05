1cd3134a6313b7f4cc4a777dc573bae9f056131b
ryancortez <ryancortez@mac.com>
2016-07-05 12:10:02 -0500

    Initial Commit

---
+++ Stocks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stocks
+//
+//  Created by Ryan Cortez on 7/5/16.
+//  Copyright © 2016 Ryan Cortez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
